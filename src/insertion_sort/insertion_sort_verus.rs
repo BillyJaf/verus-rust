32b85b70cba5a3d3041007636e3002e31084b0e8
Billy Jaffray <billyjaf@outlook.com>
2026-02-10 20:30:00 +1030

    feat: added version without ref

--- src/insertion_sort/insertion_sort_verus.rs
+++ src/insertion_sort/insertion_sort_verus.rs
@@ -8,29 +8,55 @@ verus!{
         forall |i: int| 0 <= i <= (array.len() - 1) ==> #[trigger] array.index(i) <= array.index(i + 1)
     }
 
-    pub open spec fn has_same_elements(array1: Seq<i32>, array2: Seq<i32>) -> bool
+    pub open spec fn is_permutation(array1: Seq<i32>, array2: Seq<i32>) -> bool
     {
         array1.to_multiset() == array2.to_multiset()
     }
 
-    fn sort(input_array: &mut Vec<i32>)
-        requires
-            1 < old(input_array).len() <= (i32::MAX)
-        ensures
-            has_same_elements(input_array@, old(input_array)@),
-    {
-        let mut i = 1;
+    // fn sort(input_array: &mut Vec<i32>)
+    //     requires
+    //         1 < old(input_array).len() <= (i32::MAX)
+    //     ensures
+    //         is_permutation(input_array@, old(input_array)@),
+    // {
+    //     let mut i = 1;
 
-        while i < input_array.len() 
-        {
-            let current_element = input_array[i];
-            let mut j = i as i32 - 1;
-            while j >= 0  && input_array[j as usize] > current_element {
-                input_array[j as usize + 1] = input_array[j as usize];
-                input_array[j as usize] = current_element;
-                j -= 1;
-            }
-            i += 1;
-        }
-    }   
+    //     while i < input_array.len() 
+    //     {
+    //         let current_element = input_array[i];
+    //         let mut j = i as i32 - 1;
+    //         while j >= 0  && input_array[j as usize] > current_element {
+    //             input_array[j as usize + 1] = input_array[j as usize];
+    //             input_array[j as usize] = current_element;
+    //             j -= 1;
+    //         }
+    //         i += 1;
+    //     }
+    // }   
+
+    // fn inner_loop(input_array: &mut Vec<i32>, current_element: i32, current_element_index: usize)
+    //     requires
+    //         1 < old(input_array).len() <= (i32::MAX),
+    //         0 <= current_element_index < old(input_array).len(),
+    //         old(input_array)@.contains(current_element),
+    //     ensures
+    //         is_permutation(input_array@, old(input_array)@),
+    // {
+    //     if current_element_index == 0 {
+    //         return
+    //     }
+
+    //     let mut i = current_element_index - 1;
+
+    //     while i > 0 && input_array[(i as usize)] > current_element 
+    //         invariant
+    //             i < current_element_index < input_array.len()
+    //         decreases
+    //             i + 1
+    //     {
+    //         swap_two_elements(&mut input_array, i, i+1);
+    //         i -= 1;
+    //     }
+
+    // }   
 }
--- src/swap_elements/swap_elements_once.rs
+++ src/swap_elements/swap_elements_once.rs
@@ -13,7 +13,7 @@ verus!{
         old_array.to_multiset() == new_array.to_multiset()
     }
 
-    pub fn swap_two_elements(array: &mut Vec<i32>, index_1: usize, index_2: usize)
+    pub fn swap_two_elements_ref(array: &mut Vec<i32>, index_1: usize, index_2: usize)
         requires
             index_1 < old(array).len(),
             index_2 < old(array).len(),
@@ -45,5 +45,45 @@ verus!{
         };
 
         assert(old(array)@.to_multiset() == array@.to_multiset());
+    }
+
+    pub fn swap_two_elements(array: Vec<i32>, index_1: usize, index_2: usize) -> (swapped_array: Vec<i32>)
+        requires
+            index_1 < array.len(),
+            index_2 < array.len(),
+        ensures
+            only_swapped_elements(array@, swapped_array@, index_1 as int, index_2 as int)
+    {   
+        let ghost old_array = array@;
+
+        let mut swapped_array = array.clone();
+
+        let temp_1 = swapped_array[index_1];
+        let temp_2 = swapped_array[index_2];
+        swapped_array[index_1] = temp_2;
+
+        assert(swapped_array@ == old_array.update(index_1 as int, temp_2));
+        assert(swapped_array@.to_multiset() == old_array.update(index_1 as int, temp_2).to_multiset());
+
+        assert(old_array.to_multiset().insert(temp_2).remove(old_array[index_1 as int]) == old_array.update(index_1 as int, temp_2).to_multiset()) by {
+            to_multiset_update(old_array, index_1 as int, temp_2)
+        };
+
+        swapped_array[index_2] = temp_1;
+
+        assert(swapped_array@ == old_array.update(index_1 as int, temp_2).update(index_2 as int, temp_1));
+
+        assert(old_array.update(index_1 as int, temp_2).update(index_2 as int, temp_1).to_multiset() == old_array.to_multiset().insert(temp_2).remove(temp_1).insert(temp_1).remove(temp_2)) by { 
+            to_multiset_update(old_array.update(index_1 as int, temp_2), index_2 as int, temp_1);
+            to_multiset_update(old_array, index_1 as int, temp_2)
+        };
+
+        assert(old_array.to_multiset() == old_array.to_multiset().insert(temp_2).remove(temp_1).insert(temp_1).remove(temp_2)) by {
+            old_array.to_multiset_ensures();
+        };
+
+        assert(old_array.to_multiset() == swapped_array@.to_multiset());
+
+        swapped_array
     }
 }

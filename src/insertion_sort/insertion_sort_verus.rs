5123b02686bfd9393fc7c2e0a23c92c9ed4c1463
Billy Jaffray <billyjaf@outlook.com>
2026-02-10 21:29:12 +1030

    WIP: added sorting and recursion to inner loop

--- src/insertion_sort/insertion_sort_verus.rs
+++ src/insertion_sort/insertion_sort_verus.rs
@@ -3,9 +3,25 @@ use vstd::prelude::*;
 include!("../swap_elements/swap_elements_once.rs");
 
 verus!{
+    proof fn is_sorted_recursive(array: Seq<i32>, i: int, j: int) 
+        requires
+            0 < i < j <= array.len()
+        ensures
+            is_sorted(array.subrange(i,j)) ==> is_sorted(array.subrange(i,j - 1))
+        decreases
+            j
+    {
+        if i + 1 == j {}
+        else { is_sorted_recursive(array, i, j - 1) }
+    }
+
     pub open spec fn is_sorted(array: Seq<i32>) -> bool
     {   
-        forall |i: int| 0 <= i <= (array.len() - 1) ==> #[trigger] array.index(i) <= array.index(i + 1)
+        if array.len() <= 1 {
+            true
+        } else {
+            forall |i: int| 0 <= i < array.len() - 1 ==> #[trigger] array.index(i) <= array.index(i + 1)
+        }
     }
 
     pub open spec fn is_permutation(array1: Seq<i32>, array2: Seq<i32>) -> bool
@@ -39,8 +55,10 @@ verus!{
             1 < input_array.len() <= (i32::MAX),
             0 <= current_element_index < input_array.len(),
             input_array@.contains(current_element),
+            is_sorted(input_array@.subrange(0,current_element_index as int))
         ensures
             is_permutation(input_array@, swapped_array@),
+            is_sorted(swapped_array@.subrange(0,current_element_index as int + 1))
     {
         if current_element_index == 0 {
             return input_array;
@@ -50,22 +68,33 @@ verus!{
 
         let mut i = current_element_index - 1;
 
+        assert(is_sorted(swapped_array@.subrange(0,i as int))) by {
+            assert(swapped_array.len() > current_element_index as int > 0);
+            is_sorted_recursive(swapped_array@, 0, current_element_index as int)
+        };
+
         while i > 0 && input_array[i as usize] > current_element 
             invariant
                 is_permutation(input_array@, swapped_array@),
                 0 <= i < current_element_index < swapped_array.len(),
                 swapped_array.len() == input_array.len(),
-                is_permutation(input_array@, swapped_array@),
+                is_sorted(swapped_array@.subrange(0,i as int)),
+                is_sorted(swapped_array@.subrange(i as int, current_element_index as int + 1))
             decreases
                 i + 1
         {
             swapped_array = swap_two_elements(swapped_array, i, i+1);
+            assume(is_sorted(swapped_array@.subrange(0 as int,i as int)));
+            assume(is_sorted(swapped_array@.subrange(i as int,current_element_index as int + 1)));
             i -= 1;
         }
 
         if i == 0 && swapped_array[i as usize] > current_element {
             swapped_array = swap_two_elements(swapped_array, 0, 1);
+            assume(is_sorted(swapped_array@.subrange(0 as int,current_element_index as int + 1)));
         }
+
+        assert(is_sorted(swapped_array@.subrange(0,current_element_index as int + 1)));
 
         swapped_array
 
